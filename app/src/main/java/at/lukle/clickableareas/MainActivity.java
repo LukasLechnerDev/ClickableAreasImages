611fa06d2012a1f6c04d3f6a65d68c027355915f
Lukas <lukas@tailored-apps.com>
2015-10-24 13:59:57 +0200

    added breaking bad characters as sample image

--- app/src/main/java/at/lukle/clickableareas/MainActivity.java
+++ app/src/main/java/at/lukle/clickableareas/MainActivity.java
@@ -3,7 +3,6 @@ package at.lukle.clickableareas;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -26,11 +25,11 @@ public class MainActivity extends AppCompatActivity implements OnClickableAreaCl
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         ImageView image = (ImageView) findViewById(R.id.imageView);
-        image.setImageResource(R.drawable.flugblatt);
+        image.setImageResource(R.drawable.breaking_bad_people);
 
         attacher = new PhotoViewAttacher(image);
         ClickableAreasImage clickableAreasImage = new ClickableAreasImage(attacher, this);
@@ -46,19 +45,15 @@ public class MainActivity extends AppCompatActivity implements OnClickableAreaCl
         int cellHeight = 439;
         List<ClickableArea> clickableAreas = new ArrayList<>();
 
-        clickableAreas.add(new ClickableArea(0,0,cellWidth,cellHeight,"Knacker"));
-        clickableAreas.add(new ClickableArea(cellWidth,0,cellWidth,cellHeight,"Knacker"));
-        clickableAreas.add(new ClickableArea(cellWidth*2,0,cellWidth,cellHeight,"Rostbratwürstel"));
-
-        clickableAreas.add(new ClickableArea(0,cellHeight,cellWidth,cellHeight,"Farmerschinken"));
-        clickableAreas.add(new ClickableArea(cellWidth,cellHeight,cellWidth,cellHeight,"Schwarzwälder"));
-        clickableAreas.add(new ClickableArea(cellWidth*2,cellHeight,cellWidth,cellHeight,"Putenbrust"));
-
-        clickableAreas.add(new ClickableArea(0,cellHeight*2,cellWidth,cellHeight,"Putenschinken"));
-        clickableAreas.add(new ClickableArea(cellWidth, cellHeight * 2, cellWidth, cellHeight, "Schärdinger Österkron"));
-
-        Person heisenberg = new Person("Walter","White");
-        clickableAreas.add(new ClickableArea(cellWidth*2,cellHeight*2,cellWidth,cellHeight,heisenberg));
+        clickableAreas.add(new ClickableArea(65,367,191,273,new Person("Gustavo","Fring")));
+        clickableAreas.add(new ClickableArea(257,483,151,251,new Person("Saul","Goodman")));
+        clickableAreas.add(new ClickableArea(351,277,107,167,new Person("Mike","Ehrmantraut")));
+        clickableAreas.add(new ClickableArea(459,371,95,145,new Person("Jesse","Pinkman")));
+        clickableAreas.add(new ClickableArea(557,241,225,574,new Person("Walter","\"Heisenberg\" White")));
+        clickableAreas.add(new ClickableArea(889,553,133,235,new Person("Hank","Schrader")));
+        clickableAreas.add(new ClickableArea(987,429,105,129,new Person("Marie","Schrader")));
+        clickableAreas.add(new ClickableArea(1075,299,173,379,new Person("Skylar","White")));
+        clickableAreas.add(new ClickableArea(1260,441,257,325,new Person("Walter JR","\"Flynn\" White")));
 
         return clickableAreas;
     }
